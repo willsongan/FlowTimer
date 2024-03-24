fe9e00537a25af405966fb3a5dbbfa83d76a8410
Willson <willsongan30@gmail.com>
2024-03-24 18:17:09 +0700

    Add Starting Focus Timer

--- Models/Timer.cs
+++ Models/Timer.cs
@@ -4,48 +4,64 @@ namespace FlowTimer.Models;
 
 public class Timer : ReactiveObject
 {
-    private int _hours;
-    private int _minutes;
-    private int _seconds;
+    private int? _hours;
+    private int? _minutes;
+    private int? _seconds;
     
-    public int Hours
+    public int? Hours
     {
         get => _hours;
         set
         {
             this.RaiseAndSetIfChanged(ref _hours, value);
-            if (_hours >= 24)
-            {
-                this.RaiseAndSetIfChanged(ref _hours, 0);
-            }
         }
     }
 
-    public int Minutes
+    public int? Minutes
     {
         get => _minutes;
         set
         {
-            this.RaiseAndSetIfChanged(ref _minutes, value);
-            if (_minutes > 59)
+            if (value > 59)
             {
-                _hours++;
+                Hours++;
                 this.RaiseAndSetIfChanged(ref _minutes, 0);
+            }
+            else if (value < 0)
+            {
+                if (Hours == 0) return;
+                Hours--;
+                this.RaiseAndSetIfChanged(ref _minutes, 59);
+            }
+            else
+            {
+                this.RaiseAndSetIfChanged(ref _minutes, value);
             }
         }
     }
 
-    public int Seconds
+    public int? Seconds
     {
         get => _seconds;
         set
         {
-            this.RaiseAndSetIfChanged(ref _seconds, value);
-            if (_seconds > 59)
+            if (value > 59)
             {
                 Minutes++;
                 this.RaiseAndSetIfChanged(ref _seconds, 0);
             }
+            else if (value < 0)
+            {
+                if (Hours == 0 && Minutes == 0) return;
+                Minutes--;
+                this.RaiseAndSetIfChanged(ref _seconds, 59);
+            }
+            else
+            {
+                this.RaiseAndSetIfChanged(ref _seconds, value);
+            }
         }
     }
+
+    public bool HasRanOut => Hours <= 0 && Minutes <= 0 && Seconds <= 0;
 }
--- ViewModels/TimerViewModel.cs
+++ ViewModels/TimerViewModel.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Windows.Input;
 using Avalonia.Threading;
 using FlowTimer.Models;
 using ReactiveUI;
@@ -9,7 +10,16 @@ public class TimerViewModel : ViewModelBase
 {
     public TimerViewModel()
     {
-        Timer = new()
+        WorkTimeSpan = new TimeSpan(0, 8, 0, 0);
+        WorkTimeLeft = new Timer
+        {
+            Hours = 0,
+            Minutes = 0,
+            Seconds = 0
+        };
+
+        FocusTimeSpan = new TimeSpan(0, 0, 15, 0);
+        FocusTimeLeft = new Timer()
         {
             Hours = 0,
             Minutes = 0,
@@ -17,19 +27,82 @@ public class TimerViewModel : ViewModelBase
         };
         
         _dispatcherTimer = new DispatcherTimer(TimeSpan.FromSeconds(1),DispatcherPriority.Normal,Tick);
-        _dispatcherTimer.Start();
+
+        SetWorkHourCommand = ReactiveCommand.Create(SetWorkHour);
+        StartFocusCommand = ReactiveCommand.Create(StartFocus);
     }
 
     private DispatcherTimer _dispatcherTimer;
-    private Timer _timer;
-    public Timer Timer
+    
+    private Timer? _workTimeLeft;
+    public Timer? WorkTimeLeft
     {
-        get => _timer;
-        private set => this.RaiseAndSetIfChanged(ref _timer, value);
+        get => _workTimeLeft;
+        private set => this.RaiseAndSetIfChanged(ref _workTimeLeft, value);
+    }
+
+    private TimeSpan? _workTimeSpan;
+    public TimeSpan? WorkTimeSpan
+    {
+        get => _workTimeSpan;
+        set => this.RaiseAndSetIfChanged(ref _workTimeSpan, value);
+    }
+
+    private Timer? _focusTimeLeft;
+    public Timer? FocusTimeLeft
+    {
+        get => _focusTimeLeft;
+        set => this.RaiseAndSetIfChanged(ref _focusTimeLeft, value);
+    }
+
+    private TimeSpan? _focusTimeSpan;
+    public TimeSpan? FocusTimeSpan
+    {
+        get => _focusTimeSpan;
+        set => this.RaiseAndSetIfChanged(ref _focusTimeSpan, value);
+    }
+
+    public ICommand SetWorkHourCommand { get; }
+    private void SetWorkHour()
+    {
+        if (WorkTimeSpan == null) return;
+        
+        var timeSpanValue = WorkTimeSpan.Value;
+        if (WorkTimeLeft != null)
+        {
+            WorkTimeLeft.Hours = timeSpanValue.Hours;
+            WorkTimeLeft.Minutes = timeSpanValue.Minutes;
+            WorkTimeLeft.Seconds = timeSpanValue.Seconds;
+        }
+    }
+    
+    public ICommand StartFocusCommand { get; }
+    private void StartFocus()
+    {
+       _dispatcherTimer.Start();
+
+       //set focus timer
+       if (FocusTimeSpan == null) return;
+       
+       var timeSpan = FocusTimeSpan.Value;
+       if (FocusTimeLeft != null)
+       {
+           FocusTimeLeft.Hours = timeSpan.Hours;
+           FocusTimeLeft.Minutes = timeSpan.Minutes;
+           FocusTimeLeft.Seconds = timeSpan.Seconds;
+       }
     }
 
     private void Tick(object? sender, EventArgs e)
     {
-        Timer.Seconds++;
+        if (FocusTimeLeft != null)
+        {
+            FocusTimeLeft.Seconds--;
+
+            if (FocusTimeLeft.HasRanOut)
+            {
+                _dispatcherTimer.Stop();
+            }
+        }
     }
 }
